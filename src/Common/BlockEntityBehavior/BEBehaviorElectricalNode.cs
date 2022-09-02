e99290cc9ada31fab7096e79c303779a5779fa3e
Craluminum2413 <beware.14b@gmail.com>
2022-09-02 23:39:10 +0300

    Add two extra methods to make class more human readable

--- src/Common/BlockEntityBehavior/BEBehaviorElectricalNode.cs
+++ src/Common/BlockEntityBehavior/BEBehaviorElectricalNode.cs
@@ -20,9 +20,8 @@ namespace ElectricalRevolution
 {
 	public class BEBehaviorElectricalNode : BlockEntityBehavior
 	{
-		public float Power
-		{get{return Voltage * Current;}}//in Joules
-		public float Resistance; //in Ohms
+    public float Power => Voltage * Current;//in Joules
+    public float Resistance; //in Ohms
 		public float ParasiticResistance; //Resistance converted into heat
 		public float Voltage; //in Volts
 		public float SeriesCapacitance; //in Farads
@@ -61,33 +60,19 @@ namespace ElectricalRevolution
 			}
 
 
-			if(forPlayer.WorldData.CurrentGameMode == EnumGameMode.Creative)
-			{
-			voltmeter=ammeter=calculator=ohmmeter=faradmeter=henrymeter=thermometer = true;
-			}else{
-			if(helditem?.Attributes != null) {checkrighthand = true; rightattributes = helditem.Attributes;}
-			if(offhanditem?.Attributes != null) {checklefthand = true; leftattributes = offhanditem.Attributes;}
-			if((checkrighthand && rightattributes.HasAttribute("voltmeter"))||(checklefthand && leftattributes.HasAttribute("voltmeter"))){voltmeter = true;}
-			if((checkrighthand && rightattributes.HasAttribute("ammeter"))||(checklefthand && leftattributes.HasAttribute("ammeter"))){ammeter = true;}
-			if((checkrighthand && rightattributes.HasAttribute("calculator"))||(checklefthand && leftattributes.HasAttribute("calculator"))){calculator = true;}
-			if((checkrighthand && rightattributes.HasAttribute("ohmmeter"))||(checklefthand && leftattributes.HasAttribute("ohmmeter"))){ohmmeter = true;}
-			if((checkrighthand && rightattributes.HasAttribute("faradmeter"))||(checklefthand && leftattributes.HasAttribute("faradmeter"))){faradmeter = true;}
-			if((checkrighthand && rightattributes.HasAttribute("henrymeter"))||(checklefthand && leftattributes.HasAttribute("henrymeter"))){henrymeter = true;}
-			if((checkrighthand && rightattributes.HasAttribute("thermometer"))||(checklefthand && leftattributes.HasAttribute("thermometer"))){thermometer = true;}
-			}
-			if(voltmeter){ sb.AppendFormat(Lang.Get("Node: {0}V", new object[] { this.Voltage }), Array.Empty<object>()).AppendLine(); }
-			if(ammeter){ sb.AppendFormat(Lang.Get("Node: {0}A", new object[] { this.Current }), Array.Empty<object>()).AppendLine(); }
-			if(voltmeter&&ammeter&&calculator){ sb.AppendFormat(Lang.Get("Node: {0}W", new object[] { this.Voltage * this.Power }), Array.Empty<object>()).AppendLine(); }
-			if(ohmmeter){ sb.AppendFormat(Lang.Get("Node: {0}Ω", new object[] { this.Resistance }), Array.Empty<object>()).AppendLine(); }
-			if(faradmeter){ sb.AppendFormat(Lang.Get("Node: {0}F", new object[] { this.SeriesCapacitance }), Array.Empty<object>()).AppendLine(); }
-			if(henrymeter){ sb.AppendFormat(Lang.Get("Node: {0}H", new object[] { this.Inductance }), Array.Empty<object>()).AppendLine(); }
+    private void AppendMeasurementText(StringBuilder sb, string key, object unit)
+    {
+      sb.AppendFormat(Lang.Get(key, new object[] { unit }), Array.Empty<object>()).AppendLine();
+    }
 
-			if(ohmmeter&&ammeter&&calculator){ sb.AppendFormat(Lang.Get("Node: Parasitic {0}Ω", new object[] { this.ParasiticResistance }), Array.Empty<object>()).AppendLine(); }
-			if(faradmeter&&voltmeter&&calculator){ sb.AppendFormat(Lang.Get("Node: Parasitic {0}F", new object[] { this.ParasiticCapacitance }), Array.Empty<object>()).AppendLine(); }
-			if(thermometer){ sb.AppendFormat(Lang.Get("Node: {0}°c", new object[] { "0" }), Array.Empty<object>()).AppendLine(); }
-			if(!voltmeter&&!ammeter&&!ohmmeter&&!faradmeter&&!henrymeter&&!thermometer){ sb.AppendFormat(Lang.Get("Hold a meter tool in either hand to get a readout."), Array.Empty<object>()).AppendLine(); }
-		}
-		public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor world)
+    public bool HasAttribute(IPlayer player, string treeAttribute)
+    {
+      var rightHandItem = player.InventoryManager.ActiveHotbarSlot.Itemstack.Attributes;
+      var leftHandItem = player.Entity.LeftHandItemSlot.Itemstack.Attributes;
+      return rightHandItem.HasAttribute(treeAttribute) || leftHandItem.HasAttribute(treeAttribute);
+    }
+
+    public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor world)
 		{
 			Voltage = tree.GetFloat("voltage");
 			Current = tree.GetFloat("current");
